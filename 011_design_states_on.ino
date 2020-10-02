28aef7a6208a25a849239df65d9058d5b041536d
Akash keshri <72224058+akash-123-svg@users.noreply.github.com>
2020-10-02 13:32:24 +0530

    Update 011_design_states_on.ino

--- 011_design_states_on.ino
+++ 011_design_states_on.ino
@@ -1,48 +1,48 @@
-int red = 12;
-int green = 11;
-int blue = 10;
-int brightness = 250;
-int fadeAmount = 5;
+int pink = 15;
+int yellow = 12;
+int white = 10;
+int brightness = 240;
+int fadeAmount = 6;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
-  pinMode(blue, OUTPUT);
+  pinMode(pink, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(white, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
   //On state 
-  //Red blinks @10Hz 
-//  digitalWrite(red, HIGH);
+  //pink blinks @10Hz 
+//  digitalWrite(pink, HIGH);
 //  delay(100);
-//  digitalWrite(red,LOW);
+//  digitalWrite(pink,LOW);
 //  delay(100);
 
   //Off state
   //Does nothing 
 
   //Run state 
-  //Green fades then blink 0.5s 
-   analogWrite(green, brightness);
+  //yellow fades then blink 0.5s 
+   analogWrite(yellow, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness - fadeAmount;
 
   if(brightness <= 0)
   {
-    digitalWrite(green, HIGH);
+    digitalWrite(yellow, HIGH);
     delay(500);
-    digitalWrite(green, LOW);
+    digitalWrite(yellow, LOW);
     delay(500);
-    brightness = 250;
+    brightness = 240;
   }
 
   delay(30);
 
   //Sleep State
-  //Blue blink  @4Hz for 1 sec fade for 1 sec
+  // white  @4Hz for 1 sec fade for 1 sec
   
 }
